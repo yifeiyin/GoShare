441cad45a680e389ba2f6404067141977c78f058
Yifei Yin <yifeiyin@foxmail.com>
2020-02-15 11:54:26 -0500

    Coordinates and markers

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import { StyleSheet, View, Text, TouchableOpacity, SafeAreaView } from 'react-native';
-import MapView from 'react-native-maps';
+
+// DOCUMENTATION: https://github.com/react-native-community/react-native-maps#component-api
+import MapView, { Marker } from 'react-native-maps';
+
 
 export default class MapScreen extends React.Component {
+  state = {
+    markers: [
+      { coordinate: {
+        latitude: 43.78767,
+        longitude: -79.19111,
+      }, title: 'Test',description:'desdsgfdg rgrg' }
+    ]
+  }
+
   render() {
     return (
       <SafeAreaView>
         <Text>MapScreen</Text>
-        <MapView style={{ width:'100%', height: '90%', backgroundColor:'#ddd'}}
+        <MapView style={{ width: '100%', height: '90%', backgroundColor: '#ddd' }}
           initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        />
+            latitude: 43.785,
+            longitude: -79.188,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.015,
+          }}>
+          {
+            this.state.markers.map(marker => (
+              <Marker
+                coordinate={marker.coordinate}
+                title={marker.title}
+                description={marker.description}
+              />
+            ))
+          }
+        </MapView>
       </SafeAreaView>
     );
   }
