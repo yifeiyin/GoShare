6e5a7d7917dd3fa0b9d2d084d937857fa26058e8
Yifei Yin <yifeiyin@foxmail.com>
2020-02-15 11:38:28 -0500

    Working map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, SafeAreaView } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default class MapScreen extends React.Component {
   render() {
     return (
-        <View><Text>MapScreen</Text></View>
+      <SafeAreaView>
+        <Text>MapScreen</Text>
+        <MapView style={{ width:'100%', height: '90%', backgroundColor:'#ddd'}}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </SafeAreaView>
     );
   }
 }
